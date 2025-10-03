39ba68882b0035b2d1e6366d72ab186078822fdc
Damian Baćkowski <damian.backowski@chargify.com>
2025-10-03 07:51:25 +0200

    Add support for changing the Server URL by command line

--- client/main.go
+++ client/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 
 	"github.com/gorilla/websocket"
 )
 
 func main() {
+	var serverURL = flag.String("server", "http://localhost:8080", "Server URL")
+
+	flag.Parse()
+
+	fmt.Println("Connecting to", *serverURL)
+
 	c, _, err := websocket.DefaultDialer.Dial("ws://localhost:8080/ws", nil)
 	if err != nil {
 		log.Fatal("dial:", err)
